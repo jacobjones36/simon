f53660081e50401eef517af44cb143cea5dfb2ca
jacob <jacobyjones36@gmail.com>
2023-04-03 12:02:48 -0600

    db.js

--- simon/database.js
+++ simon/database.js
@@ -1,4 +1,6 @@
 const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -10,7 +12,29 @@ if (!userName) {
 const url = `mongodb+srv://cs260:cs260password@cluster0.xsgxmzv.mongodb.net/`;
 
 const client = new MongoClient(url);
+const userCollection = client.db('simon').collection('user');
 const scoreCollection = client.db('simon').collection('score');
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+function getUserByToken(token) {
+   return userCollection.findOne({ token: token });
+}
+  
+async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+}
 
 function addScore(score){
     scoreCollection.insertOne(score);
@@ -24,4 +48,11 @@ function getHighScores(){
     const cursor = scoreCollection.find(query, options);
     return cursor.toArray();
 }
-module.exports = {addScore, getHighScores};
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+    addScore,
+    getHighScores,
+  };
+  
