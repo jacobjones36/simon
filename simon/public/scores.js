7c697f5c0308d0e7be7fdc45f3886616f2d08000
jacob <jacobyjones36@gmail.com>
2023-04-03 12:16:40 -0600

    websocket

--- simon/public/scores.js
+++ simon/public/scores.js
@@ -1,11 +1,14 @@
 async function loadScores() {
   let scores = [];
   try {
+    // Get the latest high scores from the service
     const response = await fetch('/api/scores');
     scores = await response.json();
 
+    // Save the scores in case we go offline in the future
     localStorage.setItem('scores', JSON.stringify(scores));
   } catch {
+    // If there was an error then just use the last saved scores
     const scoresText = localStorage.getItem('scores');
     if (scoresText) {
       scores = JSON.parse(scoresText);
@@ -19,6 +22,7 @@ function displayScores(scores) {
   const tableBodyEl = document.querySelector('#scores');
 
   if (scores.length) {
+    // Update the DOM with the scores
     for (const [i, score] of scores.entries()) {
       const positionTdEl = document.createElement('td');
       const nameTdEl = document.createElement('td');
