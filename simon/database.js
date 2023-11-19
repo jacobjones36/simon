65b79f8c04d46f5865d128bb731b04248af4f2dc
jacob <jacobyjones36@gmail.com>
2023-11-18 18:43:55 -0700

    mine?

--- simon/database.js
+++ simon/database.js
@@ -6,6 +6,7 @@ const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
 const hostname = process.env.MONGOHOSTNAME;
 
+
 if (!userName) {
     throw Error('Database not configured. Set environment variables');
 }
